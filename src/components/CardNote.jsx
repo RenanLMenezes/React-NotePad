69c886f1170748f0081bcf6c99fe77131ed7c0ce
RenanLMenezes <renanlmenezes2003@gmail.com>
2022-02-16 21:55:43 -0300

    initial commit

---
+++ src/components/CardNote.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function CardNote() {
+    return (
+        <section>
+            <header>
+                <h3>Título</h3>
+            </header>
+            <p>Escreva sua nota...</p>
+        </section>
+    );
+}
+
+export default CardNote;
---
+++ src/components/List.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import CardNote from "./CardNote";
+
+function List() {
+    return (
+        <ul>
+            {Array.of("Trabalho", "Trabalho", "Estudo").map((category) => {
+            return(
+
+            <li>
+                <h1>{category}</h1>
+                <CardNote />
+            </li>
+            )})}
+        </ul>
+            
+    );
+}
+
+export default List;
